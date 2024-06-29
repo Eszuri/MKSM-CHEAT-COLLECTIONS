cf96bf5e630ac10e90d77b40adcdf56a41e78a95
Eszuri <ualdo296@gmail.com>
2024-06-29 12:59:59 +0700

    updtae cors

--- server/src/middleware/middleware.ts
+++ server/src/middleware/middleware.ts
@@ -5,7 +5,10 @@ import 'dotenv/config';
 
 const middleware = (app: Express) => {
     app.use(cors({
-        origin: [String(process.env.CORS_URL)],
+        origin: [String(process.env.CORS_URL),
+            "https://mksm-cheat-collections.vercel.app/",
+            "https://mksm-cheat-collections.vercel.app/api",
+            "https://mksm-cheat-collections.vercel.app/api/"],
         credentials: true
     }));
     app.use(express.urlencoded({ extended: true }));
