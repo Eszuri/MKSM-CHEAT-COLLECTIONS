c807ec1102dfd36b33aa38fb7610a56a4a5024f1
Eszuri <ualdo296@gmail.com>
2024-06-29 13:45:24 +0700

    qw

--- server/src/controller/AuthGoogle.ts
+++ server/src/controller/AuthGoogle.ts
@@ -24,10 +24,10 @@ const AuthGoogle = async (req: Request, res: Response) => {
                 if (data === null) {
                     GoogleUserModels.create({ name: name, email: email, picture: picture })
                         .then(() => {
-                            res.status(200).cookie('auth', enc, { expires: expires, httpOnly: true, secure: true }).json({ name: name, email: email, pictures: picture })
+                            res.status(200).cookie('auth', enc, { expires: expires, secure: true }).json({ name: name, email: email, pictures: picture })
                         })
                 } else {
-                    res.status(200).cookie('auth', enc, { expires: expires, secure: true, httpOnly: true }).json({ name: name, email: email, pictures: picture })
+                    res.status(200).cookie('auth', enc, { expires: expires, secure: true }).json({ name: name, email: email, pictures: picture })
                 }
             })
             .catch(err => {
--- server/src/controller/Logout.ts
+++ server/src/controller/Logout.ts
@@ -3,5 +3,5 @@ import { Request, Response } from "express";
 export default function Logout(req: Request, res: Response) {
     const expires = new Date();
     expires.setDate(expires.getDate() + 30);
-    res.clearCookie('auth', { httpOnly: true, expires: expires, secure: true }).json({ logout: true });
+    res.clearCookie('auth', { expires: expires, secure: true }).json({ logout: true });
 };
